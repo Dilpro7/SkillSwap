772f77bfa493ab50ee43e4714195d9dc139dad4d
Mangalapurapu Eswari <147641503+kutty115@users.noreply.github.com>
2026-03-19 14:36:03 +0530

    program.cs file has changed and ensure that your program.cs code is there else you can add.

--- SkillSwap.API/Program.cs
+++ SkillSwap.API/Program.cs
@@ -1,20 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using SkillSwap.API.Data;
+using SkillSwap.API.Repositories.Implementations;
+using SkillSwap.API.Repositories.Interfaces;
+using SkillSwap.API.Services.Implementations;
+using SkillSwap.API.Services.Interfaces;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<IRequestRepository, RequestRepository>();
+builder.Services.AddScoped<IRequestService, RequestService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
